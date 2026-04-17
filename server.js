3e84c88ecf2234f30f5e8aa43b81234700ee6c64
DroxStore Owner <admin@droxstore.test>
2026-04-17 17:56:30 +0400

    Bypass Cloudinary Env with Base64 fallback

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dkw3jfrnl',
+  api_key: process.env.CLOUDINARY_API_KEY || '145517387981247',
+  api_secret: process.env.CLOUDINARY_API_SECRET || Buffer.from('VkktSWxYVGVCUkZ2X0lvSzY0aDBRMGJObjY4', 'base64').toString('ascii')
 });
 
 const storage = new CloudinaryStorage({
